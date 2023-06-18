22366cb27b27786b83e2ebf13ed6ab0ede836d6e
Li Zhao <iloahz@gmail.com>
2023-06-19 01:20:41 +0800

    disable gin

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/iloahz/traefik-plugin-manual-access-control
 
-go 1.20
+go 1.19
--- mac.go
+++ mac.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Config struct {
-	Key string `json:"key,omitempty"`
+	Key string `json:"key,omitempty" yaml:"key,omitempty" toml:"key,omitempty"`
 }
 
 func CreateConfig() *Config {
@@ -22,19 +22,41 @@ type MAC struct {
 	key    *Key
 }
 
+// func (m *MAC) createServer() {
+// 	r := gin.Default()
+// 	r.Static("/", "./ui/dist")
+// 	server := &http.Server{
+// 		Addr:    fmt.Sprintf(":%d", m.config.Port),
+// 		Handler: r,
+// 	}
+// 	go func() {
+// 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+// 			fmt.Printf("listen: %s\n", err)
+// 		}
+// 	}()
+// 	go func() {
+// 		<-m.ctx.Done()
+// 		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 1*time.Second)
+// 		defer shutdownCancel()
+// 		server.Shutdown(shutdownCtx)
+// 	}()
+// }
+
 // entry New function for traefik plugin
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	key, err := NewKey(config.Key)
 	if err != nil {
 		return nil, err
 	}
-	return &MAC{
+	m := &MAC{
 		ctx:    ctx,
 		next:   next,
 		config: config,
 		name:   name,
 		key:    key,
-	}, nil
+	}
+	// m.createServer()
+	return m, nil
 }
 
 func (m *MAC) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
