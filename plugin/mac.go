11906d953fc49049e60f7272242b04425fc272bd
Li Zhao <iloahz@gmail.com>
2023-06-25 16:45:21 +0800

    update plugin checks

--- plugin/mac.go
+++ plugin/mac.go
@@ -1,13 +1,16 @@
 package plugin
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type Config struct {
-	Key string `json:"key,omitempty" yaml:"key,omitempty" toml:"key,omitempty"`
+	Server string `json:"server,omitempty" yaml:"server,omitempty" toml:"server,omitempty"`
 }
 
 func CreateConfig() *Config {
@@ -19,27 +22,99 @@ type MAC struct {
 	next   http.Handler
 	config *Config
 	name   string
-	jwt    *JWT
 }
 
-// entry New function for traefik plugin
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	jwt, err := NewJWT(config.Key)
-	if err != nil {
-		return nil, err
-	}
 	m := &MAC{
 		ctx:    ctx,
 		next:   next,
 		config: config,
 		name:   name,
-		jwt:    jwt,
 	}
 	return m, nil
 }
 
+const (
+	cookieKey = "tpmac"
+)
+
+var (
+	ErrInvalidToken = fmt.Errorf("invalid token")
+	ErrNotAllowed   = fmt.Errorf("not allowed")
+)
+
+type Request struct {
+	IP        string  `json:"ip"`
+	UserAgent string  `json:"user_agent"`
+	URL       string  `json:"url"`
+	Token     *string `json:"token,omitempty"`
+}
+
 func (m *MAC) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	// set cookie in rw
-	rw.Header().Set("Set-Cookie", fmt.Sprintf("t-mac-token=%s", m.jwt.GenerateToken()))
-	m.next.ServeHTTP(rw, req)
+	cookie, err := req.Cookie(cookieKey)
+	r := &Request{
+		IP:        req.RemoteAddr,
+		UserAgent: req.UserAgent(),
+		URL:       req.URL.String(),
+	}
+	if err != nil {
+		// generate token
+		r.Token = &cookie.Value
+		buf, err := json.Marshal(r)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res, err := http.Post(m.config.Server+"/api/token/generate", "application/json", bytes.NewBuffer(buf))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if res.StatusCode == http.StatusOK {
+			// token generated
+			defer res.Body.Close()
+			type Response struct {
+				Token string `json:"token"`
+			}
+			buf, err := io.ReadAll(res.Body)
+			if err != nil {
+				http.Error(rw, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			var token Response
+			err = json.Unmarshal(buf, &token)
+			if err != nil {
+				http.Error(rw, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			rw.Header().Add("Set-Cookie", fmt.Sprintf("%s=%s; Path=/", cookieKey, token.Token)
+			m.next.ServeHTTP(rw, req)
+		} else {
+			// token not generated
+			http.Error(rw, ErrNotAllowed.Error(), http.StatusForbidden)
+		}
+		return
+	} else {
+		// validate token
+		r.Token = &cookie.Value
+		buf, err := json.Marshal(r)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res, err := http.Post(m.config.Server+"/api/token/validate", "application/json", bytes.NewBuffer(buf))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if res.StatusCode == http.StatusOK {
+			// token valid
+			m.next.ServeHTTP(rw, req)
+			return
+		} else {
+			// token invalid
+			http.Error(rw, ErrInvalidToken.Error(), http.StatusForbidden)
+			return
+		}
+	}
 }
